28a8924e935c11ba5d1a586a1617799b3c71a4e8
youngjelee <wpdud1221@naver.com>
2022-01-04 00:27:33 +0900

    삭제

--- src/gitPractice/GitPractice.java
+++
@@ -1,8 +0,0 @@
-package gitPractice;
-
-public class GitPractice {
-	public static void main(String args[]) {
-		System.out.println("테스트입니다.");
-	}
-}
-
--- src/gitPractice/LocalTest2.java
+++
@@ -1,6 +0,0 @@
-package gitPractice;
-
-public class LocalTest2 {
-	int a = 1 ;
-	int b = 2; 
-}
